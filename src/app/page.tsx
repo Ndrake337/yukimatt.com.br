8f667d51abde841dd32359d72f0ebea494631af8
ndrake337 <matheus.felipe337@gmail.com>
2024-07-14 23:14:37 -0300

    fix(Home): Spell check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,13 +42,13 @@ export default function Home() {
     <div className="space-y-4">
       <ContentWrapper id="Dobra-1">
         <Title>Bem Vindo(a) ao Yuki Matt</Title>
-        <div className="w-40 h-40 flex flex-col items-center justify-center border-2 border-zinc-500 rounded-full">
+        <div className="flex h-40 w-40 flex-col items-center justify-center rounded-full border-2 border-zinc-500">
           <Image
             width={144}
             height={144}
             src="https://github.com/ndrake337.png"
             alt="Picture of the author"
-            className="w-36 h-36 rounded-full"
+            className="h-36 w-36 rounded-full"
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -56,21 +56,41 @@ export default function Home() {
 
           <p>
             Olá, Meu Nome é <strong>Matheus Felipe</strong>, tenho {age} anos, e
-            atuo na área de programação desde 2020, meu foco principal é na
-            área de desenvolvimento <strong>back-end</strong> mas tenho
-            conhecimentos e experiencias atuando com <strong>Front-end</strong>,
-            <strong>Analytics</strong> e <strong>Engenharia de dados</strong>.
+            atuo na área de programação desde 2020, meu foco principal é na área
+            de desenvolvimento <strong>back-end</strong> mas tenho conhecimentos
+            e experiencias atuando com <strong>Front-end</strong>,
+            <strong>Analytics</strong> e <strong>Engenharia de dados.</strong>.
           </p>
 
           <p>
             Caso goste dos projetos e currículo apresentados nesse portfólio,
             <strong>entre em contato</strong> por meio das opções abaixo.
           </p>
-          <div className="flex justify-between pt-2 gap-2 flex-wrap">
-            <TextButton name="Email" href="mailto:matheus.felipe337@gmail.com" color="pink"><Mail /></TextButton>
-            <TextButton name="Linkedin" href="https://www.linkedin.com/in/matheus-felipe-neves/" color="blue"><Linkedin /></TextButton>
-            <TextButton name="Github" href="https://github.com/Ndrake337" color="dark"><Github /></TextButton>
-            <TextButton name="Curriculo" href="#"><FileText /></TextButton>
+          <div className="flex flex-wrap justify-between gap-2 pt-2">
+            <TextButton
+              name="Email"
+              href="mailto:matheus.felipe337@gmail.com"
+              color="pink"
+            >
+              <Mail />
+            </TextButton>
+            <TextButton
+              name="Linkedin"
+              href="https://www.linkedin.com/in/matheus-felipe-neves/"
+              color="blue"
+            >
+              <Linkedin />
+            </TextButton>
+            <TextButton
+              name="Github"
+              href="https://github.com/Ndrake337"
+              color="dark"
+            >
+              <Github />
+            </TextButton>
+            <TextButton name="Curriculo" href="#">
+              <FileText />
+            </TextButton>
           </div>
         </div>
       </ContentWrapper>
@@ -80,7 +100,7 @@ export default function Home() {
       <ContentWrapper id="Dobra-2">
         <Title>Conhecimentos</Title>
         <Tabs.Root defaultValue={Knowledge[0].Area}>
-          <Tabs.List className="flex gap-4 items-center justify-center w-full">
+          <Tabs.List className="flex w-full items-center justify-center gap-4">
             {Knowledge.map((item) => {
               return (
                 <Tabs.Trigger value={item.Area} asChild key={item.Area}>
@@ -88,24 +108,33 @@ export default function Home() {
                     <item.ButtonIcon />
                   </IconButton>
                 </Tabs.Trigger>
-              )
-            })
-            }
+              );
+            })}
           </Tabs.List>
-          <div className="bg-zinc-400 px-8 py-1 gap-8 w-screen">
+          <div className="w-screen gap-8 bg-zinc-400 px-8 py-1">
             {Knowledge.map((item) => {
-              return (<Tabs.Content value={item.Area} key={item.Area}>
-                {item.Skills.map((skill) => {
-                  return (
-                    <div key={skill.Name} className="flex flex-row items-center gap-2 my-1 p-2 bg-zinc-200/25 rounded-lg w-32 truncate cursor-pointer">
-                      <Image width={28} height={28} src={skill.Icon} className="rounded-full" alt={`Icone do Conhecimento ${skill.Name}`} />
-                      <span>{skill.Name}</span>
-                    </div>
-                  )
-                })}
-              </Tabs.Content>)
-            })
-            }
+              return (
+                <Tabs.Content value={item.Area} key={item.Area}>
+                  {item.Skills.map((skill) => {
+                    return (
+                      <div
+                        key={skill.Name}
+                        className="my-1 flex w-32 cursor-pointer flex-row items-center gap-2 truncate rounded-lg bg-zinc-200/25 p-2"
+                      >
+                        <Image
+                          width={28}
+                          height={28}
+                          src={skill.Icon}
+                          className="rounded-full"
+                          alt={`Icone do Conhecimento ${skill.Name}`}
+                        />
+                        <span>{skill.Name}</span>
+                      </div>
+                    );
+                  })}
+                </Tabs.Content>
+              );
+            })}
           </div>
         </Tabs.Root>
       </ContentWrapper>
@@ -114,7 +143,11 @@ export default function Home() {
 
       <ContentWrapper id="Dobra-3">
         <Title>Projetos em Destaque</Title>
-        <RepositoryShower title={repoData?.title} description={repoData?.description} tech={repoData?.tech}/>
+        <RepositoryShower
+          title={repoData?.title}
+          description={repoData?.description}
+          tech={repoData?.tech}
+        />
       </ContentWrapper>
       <div />
     </div>
